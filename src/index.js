0988522c10e9f8724ba7d28891990865a066998c
did.healthcare <richard.braman@did.healthcare>
2022-07-04 11:58:31 -0400

    added code for ENS.  still buggy.  trying to get webpack to serve/deploy in gcloud run

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ const origin = window.location.origin;
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 
-const BACKEND_ADDR = "https://siwe-didhealthcare-backend-axzp6idmva-uc.a.run.app";
+const profileElm = document.getElementById('profile');
+const noProfileElm = document.getElementById('noProfile');
+const welcomeElm = document.getElementById('welcome');
+
+const ensLoaderElm = document.getElementById('ensLoader');
+const ensContainerElm = document.getElementById('ensContainer');
+const ensTableElm = document.getElementById('ensTable');
+
+const ensAddr = "https://api.thegraph.com/subgraphs/name/ensdomains/ens";
+const tablePrefix = `<tr><th>ENS Text Key</th><th>Value</th></tr>`;
+
+let address;
+
+const BACKEND_ADDR = "http://localhost:3000";
 async function createSiweMessage(address, statement) {
     const res = await fetch(`${BACKEND_ADDR}/nonce`, {
         credentials: 'include',
@@ -28,9 +41,63 @@ function connectWallet() {
         .catch(() => console.log('user rejected request'));
 }
 
+async function getENSMetadata(ensName) {
+    const body = JSON.stringify({
+        query: `{
+    domains(where:{ name: "${ensName}" }) {
+        name
+        resolver {
+            texts
+        }
+    }
+}`
+    });
+
+    let res = await fetch(ensAddr, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body
+    });
+
+    const wrapper = await res.json();
+    const {data} = wrapper;
+    const {domains} = data;
+    if (domains!= null){
+        let textKeys = [];
+        for (let i = 0, x = domains.length; i < x; i++) {
+            let domain = domains[i];
+            if (domain.name === ensName) {
+                textKeys = domain.resolver.texts;
+                break;
+            }
+        }
+        welcomeElm.innerHTML = `Resolving ENS Name:` + ensName;
+        const resolver = await provider.getResolver(ensName);
+
+        let nextProfile = `<tr><td>name:</td><td>${ensName}</td></tr>`;
+        for (let i = 0, x = textKeys.length; i < x; i++) {
+            nextProfile += `<tr><td>${textKeys[i]}:</td><td>${await resolver.getText(textKeys[i])}</td></tr>`
+        }
+
+        return tablePrefix + nextProfile
+    }
+    else{
+        welcomeElm.innerHTML = `ENS Did Not Resolve`;
+        return null;
+    }
+
+}
+
 async function signInWithEthereum() {
+    profileElm.classList = 'hidden';
+    noProfileElm.classList = 'hidden';
+    welcomeElm.classList = 'hidden';
+
+    address = await signer.getAddress()
     const message = await createSiweMessage(
-        await signer.getAddress(),
+        address,
         'Sign in with Ethereum to the app.'
     );
     const signature = await signer.signMessage(message);
@@ -43,14 +110,54 @@ async function signInWithEthereum() {
         body: JSON.stringify({ message, signature }),
         credentials: 'include'
     });
+
+    if (!res.ok) {
+        console.error(`Failed in getInformation: ${res.statusText}`);
+        return 
+    }
     console.log(await res.text());
+
+    displayENSProfile();
 }
 
 async function getInformation() {
     const res = await fetch(`${BACKEND_ADDR}/personal_information`, {
         credentials: 'include',
     });
-    console.log(await res.text());
+
+    if (!res.ok) {
+        console.error(`Failed in getInformation: ${res.statusText}`);
+        return 
+    }
+
+    let result = await res.text();
+    console.log(result);
+    address = result.split(" ")[result.split(" ").length - 1];
+    displayENSProfile();
+}
+
+async function displayENSProfile() {
+    const ensName = await provider.lookupAddress(address);
+
+    if (ensName) {
+        profileElm.classList = '';
+
+        welcomeElm.innerHTML = `Hello, ${ensName}`;
+        let avatar = await provider.getAvatar(ensName);
+        if (avatar) {
+            welcomeElm.innerHTML += ` <img class="avatar" src=${avatar}/>`;
+        }
+
+        ensLoaderElm.innerHTML = 'Loading...';
+        ensTableElm.innerHTML = await getENSMetadata(ensName);
+        ensLoaderElm.innerHTML = '';
+        ensContainerElm.classList = '';
+    } else {
+        welcomeElm.innerHTML = `Hello, ${address}`;
+        noProfileElm.classList = '';
+    }
+
+    welcomeElm.classList = '';
 }
 
 const connectWalletBtn = document.getElementById('connectWalletBtn');
